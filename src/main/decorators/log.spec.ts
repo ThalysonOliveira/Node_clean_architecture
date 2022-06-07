3ed762789055895f3e78843b1dccce0e51a6d3fb
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2022-06-06 23:18:07 -0300

    test:  ensure return same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -16,10 +16,7 @@ const makeController = (): Controller => {
       const httpResponse: HttpResponse = {
         statusCode: 200,
         body: {
-          email: 'any_email@email.com',
-          name: 'any_name',
-          password: 'any_password',
-          passwordConfirmation: 'any_password'
+          name: 'Thalyson'
         }
       }
       return await new Promise((resolve) => resolve(httpResponse))
@@ -50,5 +47,27 @@ describe('Log Controller Decorator', () => {
     }
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Thalyson'
+      }
+    })
   })
 })
